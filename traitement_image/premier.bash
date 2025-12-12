e702e028d31c6414f37e6e37c37d230ab0631164
edange <ewan.dange@free.fr>
2025-12-12 15:19:21 +0100

    transfert  des fichier dans le container

--- traitement_image/premier.bash
+++ traitement_image/premier.bash
@@ -1,1 +1,18 @@
 #!/usr/bin/bash
+docker container run --rm -d --name image sae103-imagick tail -f /dev/null
+docker container cp ./deuxieme.bash image:/home
+docker container exec image mkdir /home/Ntraite
+for $fic in ../*.jpeg
+do
+    docker container cp ../$fic image:/home/Ntraite/$fic
+done
+
+for $fic in ../*.png
+do
+    docker container cp ../$fic image:/home/Ntraite/$fic
+done
+
+for $fic in ../*.webp
+do
+    docker container cp ../$fic image:/home/Ntraite/$fic
+done
