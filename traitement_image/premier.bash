f9cac4d494fd9f511e853bcd75225de2fdba4f07
edange <ewan.dange@free.fr>
2025-12-12 15:27:00 +0100

    permier bash il faut tester

--- traitement_image/premier.bash
+++ traitement_image/premier.bash
@@ -2,6 +2,8 @@
 docker container run --rm -d --name image sae103-imagick tail -f /dev/null
 docker container cp ./deuxieme.bash image:/home
 docker container exec image mkdir /home/Ntraite
+docker container exec image mkdir /home/Termine
+mkdir ../ImageTraite
 for $fic in ../*.jpeg
 do
     docker container cp ../$fic image:/home/Ntraite/$fic
@@ -16,3 +18,14 @@ for $fic in ../*.webp
 do
     docker container cp ../$fic image:/home/Ntraite/$fic
 done
+
+docker container exec bash /home/deuxieme.bash
+
+fini=$(docker container exec image ls /home/Termine)
+
+for $fic in $fini
+do
+    docker container cp ../$fic image:/home/Ntraite/$fic
+done
+
+docker container stop image
