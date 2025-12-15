b6d2cc76b0590a8b50738f2022679ae29b978248
edange <ewan.dange@free.fr>
2025-12-15 09:09:41 +0100

    debut de programmation du script interne

--- traitement_image/deuxieme.bash
+++ traitement_image/deuxieme.bash
@@ -1,1 +1,7 @@
 #!/usr/bin/bash
+for $fic in ./Ntraite/*.jpeg
+do
+    tailleX=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 1
+    tailleY=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 2
+    if 
+done
