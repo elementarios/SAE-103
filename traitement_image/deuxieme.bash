ae6af9612753553911d1329327aa259cf66edf99
edange <ewan.dange@free.fr>
2026-01-05 09:20:37 +0100

    fin des correction

--- traitement_image/deuxieme.bash
+++ traitement_image/deuxieme.bash
@@ -25,7 +25,7 @@ do
             then    
                 convert ./Ntraite/$fic -resize 40% ./Termine/$nomfic.webp
  
-            elif [ (tailleX*0.2 -le 900) -a (tailleY*0.2 -le 620) -a (tailleX*0.2 -ge 350) -a (tailleY*0.2 -ge 250) ]
+            elif [ $tailleX*0.2 -le 900 ] && [ $tailleY*0.2 -le 620 ] && [ $tailleX*0.2 -ge 350 ] && [ $tailleY*0.2 -ge 250 ]
             then    
                 convert ./Ntraite/$fic -resize 20% ./Termine/$nomfic.webp
             fi
@@ -49,23 +49,23 @@ do
     dernierchar=eccho $fic | wc -c
     nomfic=eccho $fic | colrm $dernierchar-3 $dernierchar
 
-    if [ (tailleX -gt 350) -a (tailleY -gt 250) ] #on verifie que l'image n'est pas trop petite
+    if [ $tailleX -gt 350 ] && [ $tailleY -gt 250 ] #on verifie que l'image n'est pas trop petite
     then
-        if  [ (tailleX -gt 900) -a (tailleY -gt 620) ] #si l'image est trop grande 
+        if  [ $tailleX -gt 900 ] && [ $tailleY -gt 620 ] #si l'image est trop grande 
         then
-            if [ (tailleX*0.8 -le 900) -a (tailleY*0.8 -le 620) -a (tailleX*0.8 -ge 350) -a (tailleY*0.8 -ge 250) ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
+            if [ $tailleX*0.8 -le 900 ] && [ $tailleY*0.8 -le 620 ] && [ $tailleX*0.8 -ge 350 ] && [ $tailleY*0.8 -ge 250 ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
             then
                 convert ./Ntraite/$fic -resize 80% ./Termine/$nomfic.webp
 
-            elif [ (tailleX*0.6 -le 900) -a (tailleY*0.6 -le 620) -a (tailleX*0.6 -ge 350) -a (tailleY*0.6 -ge 250) ]
+            elif [ $tailleX*0.6 -le 900 ] && [ $tailleY*0.6 -le 620 ] && [ $tailleX*0.6 -ge 350 ] && [ $tailleY*0.6 -ge 250 ] 
             then    
                 convert ./Ntraite/$fic -resize 60% ./Termine/$nomfic.webp
 
-            elif [ (tailleX*0.4 -le 900) -a (tailleY*0.4 -le 620) -a (tailleX*0.4 -ge 350) -a (tailleY*0.4 -ge 250) ]
+            elif [ $tailleX*0.4 -le 900 ] && [ $tailleY*0.4 -le 620 ] && [ $tailleX*0.4 -ge 350 ] && [ $tailleY*0.4 -ge 250 ]
             then    
                 convert ./Ntraite/$fic -resize 40% ./Termine/$nomfic.webp
  
-            elif [ (tailleX*0.2 -le 900) -a (tailleY*0.2 -le 620) -a (tailleX*0.2 -ge 350) -a (tailleY*0.2 -ge 250) ]
+            elif [ $tailleX*0.2 -le 900 ] && [ $tailleY*0.2 -le 620 ] && [ $tailleX*0.2 -ge 350 ] && [ $tailleY*0.2 -ge 250 ]
             then    
                 convert ./Ntraite/$fic -resize 20% ./Termine/$nomfic.webp
             fi
@@ -85,23 +85,23 @@ do
     tailleX=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 1
     tailleY=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 2
 
-    if [ (tailleX -gt 350) -a (tailleY -gt 250) ] #on verifie que l'image n'est pas trop petite
+    if [ $tailleX -gt 350 ] && [ $tailleY -gt 250 ] #on verifie que l'image n'est pas trop petite
     then
-        if  [ (tailleX -gt 900) -a (tailleY -gt 620) ] #si l'image est trop grande 
+        if  [ $tailleX -gt 900 ] && [ $tailleY -gt 620 ] #si l'image est trop grande 
         then
-            if [ (tailleX*0.8 -le 900) -a (tailleY*0.8 -le 620) -a (tailleX*0.8 -ge 350) -a (tailleY*0.8 -ge 250) ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
+            if [ $tailleX*0.8 -le 900 ] && [ $tailleY*0.8 -le 620 ] && [ $tailleX*0.8 -ge 350 ] && [ $tailleY*0.8 -ge 250 ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
             then
                 convert ./Ntraite/$fic -resize 80% ./Termine/$fic
 
-            elif [ (tailleX*0.6 -le 900) -a (tailleY*0.6 -le 620) -a (tailleX*0.6 -ge 350) -a (tailleY*0.6 -ge 250) ]
+            elif [ $tailleX*0.6 -le 900 ] && [ $tailleY*0.6 -le 620 ] && [ $tailleX*0.6 -ge 350 ] && [ $tailleY*0.6 -ge 250 ] 
             then    
                 convert ./Ntraite/$fic -resize 60% ./Termine/$fic
 
-            elif [ (tailleX*0.4 -le 900) -a (tailleY*0.4 -le 620) -a (tailleX*0.4 -ge 350) -a (tailleY*0.4 -ge 250) ]
+            elif [ $tailleX*0.4 -le 900 ] && [ $tailleY*0.4 -le 620 ] && [ $tailleX*0.4 -ge 350 ] && [ $tailleY*0.4 -ge 250 ]
             then    
                 convert ./Ntraite/$fic -resize 40% ./Termine/$fic
  
-            elif [ (tailleX*0.2 -le 900) -a (tailleY*0.2 -le 620) -a (tailleX*0.2 -ge 350) -a (tailleY*0.2 -ge 250) ]
+            elif [ $tailleX*0.2 -le 900 ] && [ $tailleY*0.2 -le 620 ] && [ $tailleX*0.2 -ge 350 ] && [ $tailleY*0.2 -ge 250 ]
             then    
                 convert ./Ntraite/$fic -resize 20% ./Termine/$fic
             fi
