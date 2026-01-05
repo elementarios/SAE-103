d7be5ebc1da49adab1e1e610f8bc43403a32b071
edange <ewan.dange@free.fr>
2026-01-05 09:17:55 +0100

    coorection des test

--- traitement_image/premier.bash
+++ traitement_image/premier.bash
@@ -1,26 +1,30 @@
 #!/usr/bin/bash
-docker container run -d --name image sae103-imagick sleep 300 #changer  en bigpapoo/sae103-imagick si vous n'etes pas a l'iut
+docker run -dit --name image sae103-imagick  #changer  en bigpapoo/sae103-imagick si vous n'etes pas a l'iut
 docker container cp ./traitement_image/deuxieme.bash image:/home
 docker container exec image mkdir /home/Ntraite
 docker container exec image mkdir /home/Termine
 mkdir ../ImageTraite
 
-for $fic in ../*.jpeg
+ficpng=ls ./*.png
+ficjpeg=ls ./*.jpg
+ficwebp=ls ./*.webp
+
+for $fic1 in $ficpng
 do
-    docker container cp ../$fic image:/home/Ntraite/$fic
+    docker container cp ../$fic1 image:/home/Ntraite/$fic1
 done
 
-for $fic in ../*.png
+for $fic2 in $ficjpeg
 do
-    docker container cp ../$fic image:/home/Ntraite/$fic
+    docker container cp ../$fic2 image:/home/Ntraite/$fic2
 done
 
-for $fic in ../*.webp
+for $fic3 in $ficwebp
 do
-    docker container cp ../$fic image:/home/Ntraite/$fic
+    docker container cp ../$fic3 image:/home/Ntraite/$fic3
 done
 
-docker container exec bash image:/home/deuxieme.bash
+docker container exec image bash /home/deuxieme.bash
 
 
 fini=$(docker container exec image ls /home/Termine)
@@ -32,3 +36,4 @@ do
 done
 
 docker container stop image
+docker container rm image
