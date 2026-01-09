581b5bb19368e82aa81d71eb2dd5be0ec8034a54
edange <ewan.dange@free.fr>
2026-01-09 09:38:03 +0100

    optimisation du code

--- traitement_image/deuxieme.bash
+++ traitement_image/deuxieme.bash
@@ -4,7 +4,7 @@ ficpng=$(ls /home/Ntraite/*.png)
 ficwebp=$(ls /home/Ntraite/*.webp)
 ficpdf=$(ls /home/Ntraite/*.webp)
 
-for fic in $ficjpg
+for fic in $ficjpg $ficpdf $ficpng
 do
     #pour obtenir la taille de l'image
     tailleX=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 1
@@ -44,45 +44,7 @@ do
     fi
 done
 
-for fic in $ficpng
-do
-    #pour obtenir la taille de l'image
-    tailleX=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 1
-    tailleY=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 2
 
-    #pour obtenir le nom de l'image sans l'extension
-    dernierchar=eccho $fic | wc -c
-    nomfic=eccho $fic | colrm $dernierchar-4 $dernierchar
-
-    if [ $tailleX -gt 350 ] && [ $tailleY -gt 250 ] #on verifie que l'image n'est pas trop petite
-    then
-        if  [ $tailleX -gt 900 ] && [ $tailleY -gt 620 ] #si l'image est trop grande 
-        then
-            if [ $tailleX*0.8 -le 900 ] && [ $tailleY*0.8 -le 620 ] && [ $tailleX*0.8 -ge 350 ] && [ $tailleY*0.8 -ge 250 ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
-            then
-                convert /home/Ntraite/$fic -resize 80% /home/Termine/$nomfic.webp
-
-            elif [ $tailleX*0.6 -le 900 ] && [ $tailleY*0.6 -le 620 ] && [ $tailleX*0.6 -ge 350 ] && [ $tailleY*0.6 -ge 250 ] 
-            then    
-                convert /home/Ntraite/$fic -resize 60% /home/Termine/$nomfic.webp
-
-            elif [ $tailleX*0.4 -le 900 ] && [ $tailleY*0.4 -le 620 ] && [ $tailleX*0.4 -ge 350 ] && [ $tailleY*0.4 -ge 250 ]
-            then    
-                convert /home/Ntraite/$fic -resize 40% /home/Termine/$nomfic.webp
- 
-            elif [ $tailleX*0.2 -le 900 ] && [ $tailleY*0.2 -le 620 ] && [ $tailleX*0.2 -ge 350 ] && [ $tailleY*0.2 -ge 250 ]
-            then    
-                convert /home/Ntraite/$fic -resize 20% /home/Termine/$nomfic.webp
-            fi
-
-
-        else #l'image possede les bonne dimensions
-            convert /home/Ntraite/$fic /home/Termine/$nomfic.webp
-
-        fi
-
-    fi
-done
 
 for fic in $ficwebp
 do
@@ -120,42 +82,3 @@ do
     fi
 done
 
-for fic in $ficpdf
-do
-    #pour obtenir la taille de l'image
-    tailleX=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 1
-    tailleY=identify $fic | cut -d ' ' -f 3 | cut -d 'x' -f 2
-
-    #pour obtenir le nom de l'image sans l'extension
-    dernierchar=eccho $fic | wc -c
-    nomfic=eccho $fic | colrm $dernierchar-4 $dernierchar
-
-    if [ $tailleX -gt 350 ] && [ $tailleY -gt 250 ] #on verifie que l'image n'est pas trop petite
-    then
-        if  [ $tailleX -gt 900 ] && [ $tailleY -gt 620 ] #si l'image est trop grande 
-        then
-            if [ $tailleX*0.8 -le 900 ] && [ $tailleY*0.8 -le 620 ] && [ $tailleX*0.8 -ge 350 ] && [ $tailleY*0.8 -ge 250 ] #on verifie que pour certain ratio l'image correspondent au bonne dimensions
-            then
-                convert /home/Ntraite/$fic -resize 80% /home/Termine/$nomfic.webp
-
-            elif [ $tailleX*0.6 -le 900 ] && [ $tailleY*0.6 -le 620 ] && [ $tailleX*0.6 -ge 350 ] && [ $tailleY*0.6 -ge 250 ] 
-            then    
-                convert /home/Ntraite/$fic -resize 60% ./Termine/$nomfic.webp
-
-            elif [ $tailleX*0.4 -le 900 ] && [ $tailleY*0.4 -le 620 ] && [ $tailleX*0.4 -ge 350 ] && [ $tailleY*0.4 -ge 250 ]
-            then    
-                convert /home/Ntraite/$fic -resize 40% /home/Termine/$nomfic.webp
- 
-            elif [ $tailleX*0.2 -le 900 ] && [ $tailleY*0.2 -le 620 ] && [ $tailleX*0.2 -ge 350 ] && [ $tailleY*0.2 -ge 250 ]
-            then    
-                convert /home/Ntraite/$fic -resize 20% /home/Termine/$nomfic.webp
-            fi
-
-
-        else #l'image possede les bonne dimensions
-            convert /home/Ntraite/$fic /home/Termine/$nomfic.webp
-
-        fi
-
-    fi
-done
